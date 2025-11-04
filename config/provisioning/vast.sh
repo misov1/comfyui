c8472aac90565385505231d67149a3b491deda88
misov_ <70870640+misov1@users.noreply.github.com>
2025-11-05 03:02:52 +0900

    Update vast.sh

--- config/provisioning/vast.sh
+++ config/provisioning/vast.sh
@@ -43,19 +43,18 @@ NODES=(
 )
 
 WORKFLOWS=(
-    "https://raw.githubusercontent.com/misov1/comfyui/refs/heads/main/9411e2d2d0bf3b676795561584aaf097a9186c797b2ca20dc6c9c13e79843c30.png"
 )
 
 CHECKPOINT_MODELS=(
     "https://huggingface.co/baqu2213/PoemForSmallFThings/resolve/main/NAI-XL_vpred1.0_2dac_colorized.safetensors" # NoobAI-XL custom merge model
 #    "https://huggingface.co/baqu2213/PoemForSmallFThings/resolve/main/NAI-XL_vpred1.0_2dac_colorized_style2.safetensors" # NoobAI-XL custom merge model+ color 2D Str
-    "https://civitai.com/api/download/models/2289321?type=Archive&format=Other&token=5fb37fbd25ef6e4438561cfde9650f12" # Smooth Mix Wan 2.2
-    "https://huggingface.co/Bedovyy/smoothMixWan22-I2V-GGUF/resolve/main/HighNoise/smoothMixWan22I2VT2V_i2vHigh-Q8_0.gguf" # smoothMixWan2.2 I2V Q8_High
-    "https://huggingface.co/Bedovyy/dasiwaWAN22I2V14B-GGUF/resolve/LureNoir/LowNoise/dasiwaWAN22I2V14B_lurenoirLow-Q8_0.gguf" #dasiwaWan2.2 I2V Q8_Low
-
 )
 
 UNET_MODELS=(
+    "https://huggingface.co/Bedovyy/smoothMixWan22-I2V-GGUF/resolve/main/HighNoise/smoothMixWan22I2VT2V_i2vHigh-Q8_0.gguf" # smoothMixWan2.2 I2V Q8_High
+    "https://huggingface.co/Bedovyy/dasiwaWAN22I2V14B-GGUF/resolve/LureNoir/LowNoise/dasiwaWAN22I2V14B_lurenoirLow-Q8_0.gguf" #dasiwaWan2.2 I2V Q8_Low
+    "https://huggingface.co/chatpig/encoder/resolve/main/umt5_xxl_fp8_e4m3fn_scaled.safetensors"
+    "https://huggingface.co/calcuis/wan-gguf/resolve/f52f5a1f0ba441d50277fb7cdd7c1b36611837f9/clip_vision_h.safetensors"
 )
 
 LORA_MODELS=(
@@ -65,6 +64,7 @@ LORA_MODELS=(
 )
 
 VAE_MODELS=(
+    "https://huggingface.co/calcuis/wan-gguf/resolve/2f41e77bfc957eab2020821463d0cd7b15804bb9/wan_2.1_vae.safetensors"
 )
 
 UPSCALE_MODELS=(
